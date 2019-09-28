9a322ba1e0d9bbecd606896f16ea5d09b9198abf
Unknown <ajlabozz@yahoo.com>
2019-09-28 14:35:07 -0400

    Database has been created with migrations folder deployed to app directory

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,6 @@
 This is the test for Leslie and A.J.'s 499 project for the Office of Sustainability.
+
+Pip install this shizz in your Venv:
+flask-sqlalchemy
+flask-migrate
+python dot-env
--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this'
-
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+	#disabled tack_modification to not signal the application every time a change is about to be made to the database.
+	SQLALCHEMY_TRACK_MODIFICATIONS = False 
+	
     # Enable Flask's debugging features. Should be False in production
-    DEBUG = True
+	DEBUG = True
