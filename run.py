59aaa88e2f735111e75b7205e8ba8335ac84ec4c
Unknown <ajlabozz@yahoo.com>
2019-10-02 11:26:22 -0400

    Completing initial creation of database

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from app import app
+from app import app, db
+from app.models import User, Project, Goals, Roles
+
+'''
+following function adds the database instance and models to python shell sessions
+for testing purposes. This way we can work with the database entities  without having
+to import them. command: flask shell (while venv active)
+'''
+@app.shell_context_processor
+def make_shell_context():
+    return{'db': db, 'User': User, 'Project': Project, 'Goals': Goals, 'Roles': Roles}
 
 if __name__ == '__main__':
     app.run()
