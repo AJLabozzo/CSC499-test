5649943a89437896bfe14bb6329831055da00e97
Unknown <ajlabozz@yahoo.com>
2019-10-22 17:19:13 -0400

    Postgresql db instance connected

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dontguessmypostgrespassword@34.73.99.129/postgres'
 	#disabled tack_modification to not signal the application every time a change is about to be made to the database.
 	SQLALCHEMY_TRACK_MODIFICATIONS = False 
 	
