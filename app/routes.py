cc6568e3efc7f14bdd6905d10acee61edca6effb
Unknown <ajlabozz@yahoo.com>
2019-10-30 15:23:10 -0400

    User profile now pulls user data from db

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 from flask import flash, render_template, request, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, ProjectSearchForm, ProjectSubmissionForm, SignupForm
+from app.forms import LoginForm, ProjectSearchForm, ProjectSubmissionForm, SignupForm, ProfileForm
 from app.models import User, Project, Goals
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -76,7 +76,8 @@ def goals1():
 @app.route('/profile/<username>')
 def profile(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	return render_template('myprofile.html', user=user)
+	projects = Project.query.filter_by(user_id = user.id).all()
+	return render_template('myprofile.html', user=user, projects=projects)
 	
 	
 @app.route('/submission_form', methods=['GET', 'POST'])
@@ -101,14 +102,12 @@ def submission_form():
 def edit_profile():
     form = ProfileForm()
     if form.validate_on_submit():
-        current_user.username = form.username.data
         current_user.bio = form.about.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
+        form.about.data = current_user.bio
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
     
