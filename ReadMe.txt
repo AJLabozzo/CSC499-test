fb44ff5a41c9c038b64791ae3ffe33a583b7b0f0
Unknown <ajlabozz@yahoo.com>
2019-11-24 17:05:36 -0500

    removed some files, made admin page only accessible to admins

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,10 +1,2 @@
 This is the test for Leslie and A.J.'s 499 project for the Office of Sustainability.
 
-Pip install this shizz in your Venv:
-
-flask-sqlalchemy
-flask-migrate
-flask-login
-flask-table
-
-python dot-env
--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
 	user_role = db.Column(db.String(64))
-	admin = db.Column(db.Boolean, unique=False, default=False)
+	admin = db.Column(db.Boolean)
 	fname = db.Column(db.String(64))
 	lname = db.Column(db.String(64))
 	major = db.Column(db.String(64))
